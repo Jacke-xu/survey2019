e8fc3c4802f9c05a9a7cb3bca38db5e96efeb8b6
moxiaoyan <994355869@qq.com>
2019-05-26 10:50:22 +0800

    popOver

---
+++ 03_PopOver/03_PopOverTests/_3_PopOverTests.swift
@@ -0,0 +1,34 @@
+//
+//  _3_PopOverTests.swift
+//  03_PopOverTests
+//
+//  Created by moxiaoyan on 2019/5/26.
+//  Copyright © 2019 moxiaoyan. All rights reserved.
+//
+
+import XCTest
+@testable import _3_PopOver
+
+class _3_PopOverTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
